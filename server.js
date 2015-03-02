68d3ba151ed704f3e6e9206be5e0287290f16832
Gilles Fabio <gilles@gillesfabio.com>
2015-03-02 14:04:19 +0100

    Add dead simple API powered by Express.

---
+++ server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var fs          = require('fs');
+var path        = require('path');
+var util        = require('util');
+var express     = require('express');
+var serveStatic = require('serve-static');
+var app         = module.exports.app = exports.app = express();
+
+var ROOT_DIR   = path.join(__dirname, 'sample');
+var STYLES_DIR = path.join(__dirname, 'styles');
+var DATA       = path.join(__dirname, 'sample', 'data.json');
+var NODE_PORT  = process.env.NODE_PORT || 3000;
+var NODE_ENV   = process.env.NODE_ENV || 'development';
+var PER_PAGE   = 10;
+
+
+app.use(serveStatic(ROOT_DIR));
+app.use(serveStatic(STYLES_DIR));
+
+
+function getPaginatedItems(items, page) {
+  page = page || 1;
+  var offset = (page - 1) * PER_PAGE;
+  return items.slice(offset, offset + PER_PAGE);
+}
+
+
+app.get('/comments', function(req, res) {
+
+  var data         = JSON.parse(fs.readFileSync(DATA));
+  var items        = data.comments;
+  var page         = req.query.page ? parseInt(req.query.page, 10) : 0;
+  var nextPage     = page + 1;
+  var previousPage = (page < 1) ? 1 : page - 1;
+
+  var meta = {
+    limit    : PER_PAGE,
+    next     : util.format('?limit=%s&page=%s', PER_PAGE, nextPage),
+    page     : req.query.page,
+    previous : util.format('?limit=%s&page=%s', PER_PAGE, previousPage)
+  };
+
+  var json = {
+    meta     : meta,
+    comments : getPaginatedItems(items, page)
+  };
+
+  return res.json(json);
+
+});
+
+
+app.listen(NODE_PORT, function() {
+  console.log('Server running on %s mode on port %d', NODE_ENV, NODE_PORT);
+});
