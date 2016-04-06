f212df3fc2744eafc6060382208f8cc9349479cc
tarnas14 <tarnas14@gmail.com>
2016-04-06 23:11:36 +0200

    updated formatting settings
    added esversion: 6 to .jshintrc; formatted test file to comply with 80 character line length jshint constraint

--- __tests__/PaginationBoxView-test.js
+++ __tests__/PaginationBoxView-test.js
@@ -5,7 +5,8 @@ jest.dontMock('./../react_components/PaginationBoxView');
 jest.dontMock('./../react_components/PageView');
 jest.dontMock('./../react_components/BreakView');
 
-const PaginationBoxView = require('./../react_components/PaginationBoxView').default;
+const PaginationBoxView =
+  require('./../react_components/PaginationBoxView').default;
 const PageView = require('./../react_components/PageView').default;
 const BreakView = require('./../react_components/BreakView').default;
 
@@ -20,11 +21,27 @@ describe('PaginationBoxView', () => {
   it('should render a pagination component', () => {
     expect(ReactDOM.findDOMNode(pagination).nodeName).toEqual("UL");
 
-    ReactTestUtils.scryRenderedComponentsWithType(pagination, PaginationBoxView);
+    ReactTestUtils
+      .scryRenderedComponentsWithType(pagination, PaginationBoxView);
 
-    expect(ReactDOM.findDOMNode(pagination).querySelector("li:first-child a").textContent).toBe("Previous");
-    expect(ReactDOM.findDOMNode(pagination).querySelector(".selected a").textContent).toBe("1");
-    expect(ReactDOM.findDOMNode(pagination).querySelector("li:last-child a").textContent).toBe("Next");
+    expect(
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector("li:first-child a")
+        .textContent
+    ).toBe("Previous");
+    expect(
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector(".selected a")
+        .textContent
+    ).toBe("1");
+    expect(
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector("li:last-child a")
+        .textContent
+    ).toBe("Next");
 
     const pages = ReactDOM.findDOMNode(pagination).querySelectorAll("li");
     // 3*2 margins + 1 break label + previous & next buttons == 9:
@@ -32,27 +49,47 @@ describe('PaginationBoxView', () => {
   });
 
   it('test previous and next buttons', () => {
-    let elmts = ReactTestUtils.scryRenderedDOMComponentsWithTag(pagination, 'li');
+    let elmts =
+      ReactTestUtils.scryRenderedDOMComponentsWithTag(pagination, 'li');
     let previous = elmts[0];
     let next = elmts[elmts.length - 1];
 
     ReactTestUtils.Simulate.click(next);
 
-    expect(ReactDOM.findDOMNode(pagination).querySelector(".selected a").textContent).toBe("2");
+    expect(
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector(".selected a")
+        .textContent
+    ).toBe("2");
 
     ReactTestUtils.Simulate.click(previous);
 
-    expect(ReactDOM.findDOMNode(pagination).querySelector(".selected a").textContent).toBe("1");
+    expect(
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector(".selected a")
+        .textContent
+    ).toBe("1");
   });
 
   it('test click on a page item', () => {
     ReactTestUtils.findRenderedComponentWithType(pagination, PaginationBoxView);
 
-    const pageItem = ReactDOM.findDOMNode(pagination).querySelector("li:nth-child(3)").querySelector("a");
+    const pageItem =
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector("li:nth-child(3)")
+        .querySelector("a");
 
     ReactTestUtils.Simulate.click(pageItem);
 
-    expect(ReactDOM.findDOMNode(pagination).querySelector(".selected a").textContent).toBe("2");
+    expect(
+      ReactDOM
+        .findDOMNode(pagination)
+        .querySelector(".selected a")
+        .textContent
+    ).toBe("2");
   });
 
   it('test rendering only active page item', function() {
