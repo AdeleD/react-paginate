db3a8d52b59bfd6ebd16fcc8e5406a565b4be32e
tarnas14 <tarnas14@gmail.com>
2016-04-06 23:41:52 +0200

    added tests for initialSelected props
    checking if initial selected page is forced correctly and does not change when initialSelected prop changes value

---
+++ __tests__/ForcingPaginationState-test.js
@@ -0,0 +1,56 @@
+'use-strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-addons-test-utils';
+
+jest.dontMock('./../react_components/PaginationBoxView');
+jest.dontMock('./../react_components/PageView');
+jest.dontMock('./../react_components/BreakView');
+
+const PaginationBoxView =
+  require('./../react_components/PaginationBoxView').default;
+
+describe('prop initialSelected', () => {
+  it('should force selected page when first rendering the pagination', () => {
+    const initialSelectedPageIndex = 1;
+    const forcedPagination = ReactTestUtils.renderIntoDocument(
+      <PaginationBoxView
+        pageNum={2}
+        initialSelected={initialSelectedPageIndex}
+      />
+    );
+
+    const actualSelected =
+      ReactDOM.findDOMNode(forcedPagination).querySelector('.selected a');
+    const expectedSelectedPageLabel = initialSelectedPageIndex + 1 + '';
+
+    expect(actualSelected.textContent).toBe(expectedSelectedPageLabel);
+  });
+
+  it('should not change selected page when the prop value changes', () => {
+    const initialSelectedPageIndex = 1;
+    const domNode = document.createElement('div');
+    const forcedPagination = ReactDOM.render(
+      <PaginationBoxView
+        pageNum={2}
+        initialSelected={initialSelectedPageIndex}
+      />,
+      domNode
+    );
+
+    ReactDOM.render(
+      <PaginationBoxView
+        pageNum={2}
+        initialSelected={initialSelectedPageIndex + 1}
+      />,
+      domNode
+    );
+
+    const actualSelected =
+      ReactDOM.findDOMNode(forcedPagination).querySelector('.selected a');
+    const expectedSelectedPageLabel = initialSelectedPageIndex + 1 + '';
+
+    expect(actualSelected.textContent).toBe(expectedSelectedPageLabel);
+  });
+});
