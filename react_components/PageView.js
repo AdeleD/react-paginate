bfbeb31c38a57e2866bb34d33b892f501cf8e828
Sergei Fundaev <fundaevs@gmail.com>
2017-01-31 10:02:15 +0400

    It was necessary to click exactly on page number to change page. Fixed.

--- react_components/PageView.js
+++ react_components/PageView.js
@@ -17,7 +17,7 @@ export default class PageView extends React.Component {
     }
 
     return (
-        <li className={cssClassName}>
+        <li className={cssClassName} onClick={onClick}>
             <a onClick={onClick}
                className={linkClassName}
                tabIndex="0"
