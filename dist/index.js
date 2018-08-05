9442a0407b86d0f9f3764e516798df4a88dd4f4b
Yoan Tournade <yoan@ytotech.com>
2018-08-05 13:05:31 +0200

    Use exclusively ES6 exports

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _PaginationBoxView = require('./PaginationBoxView');
 
 var _PaginationBoxView2 = _interopRequireDefault(_PaginationBoxView);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = _PaginationBoxView2.default;
+exports.default = _PaginationBoxView2.default;
 //# sourceMappingURL=index.js.map
--- react_components/index.js
+++ react_components/index.js
@@ -1,3 +1,3 @@
 import PaginationBoxView from './PaginationBoxView';
 
-module.exports = PaginationBoxView;
+export default PaginationBoxView;
