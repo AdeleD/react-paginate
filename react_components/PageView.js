731c046692e05ff08c281f266f4aaa4694a6540d
Adele Delamarche <adele.delamarche@gmail.com>
2018-11-03 11:19:51 +0100

    Add the activeLinkClassName prop

--- react_components/PageView.js
+++ react_components/PageView.js
@@ -3,10 +3,12 @@
 import React from 'react';
 
 const PageView = (props) => {
-  let cssClassName = props.pageClassName;
-  const linkClassName = props.pageLinkClassName;
+  let pageClassName = props.pageClassName;
+  let pageLinkClassName = props.pageLinkClassName;
+
   const onClick = props.onClick;
   const href = props.href;
+
   let ariaLabel = 'Page ' + props.page +
     (props.extraAriaContext ? ' ' + props.extraAriaContext : '');
   let ariaCurrent = null;
@@ -14,18 +16,29 @@ const PageView = (props) => {
   if (props.selected) {
     ariaCurrent = 'page';
     ariaLabel = 'Page ' + props.page + ' is your current page';
-    if (typeof(cssClassName) !== 'undefined') {
-      cssClassName = cssClassName + ' ' + props.activeClassName;
+
+    if (typeof(pageClassName) !== 'undefined') {
+      pageClassName = pageClassName + ' ' + props.activeClassName;
     } else {
-      cssClassName = props.activeClassName;
+      pageClassName = props.activeClassName;
+    }
+
+    if (typeof(pageLinkClassName) !== 'undefined') {
+      pageLinkClassName = pageLinkClassName;
+
+      if (typeof(props.activeLinkClassName) !== 'undefined') {
+        pageLinkClassName = pageLinkClassName + ' ' + props.activeLinkClassName;
+      }
+    } else {
+      pageLinkClassName = props.activeLinkClassName;
     }
   }
 
   return (
-      <li className={cssClassName}>
+      <li className={pageClassName}>
           <a onClick={onClick}
              role="button"
-             className={linkClassName}
+             className={pageLinkClassName}
              href={href}
              tabIndex="0"
              aria-label={ariaLabel}
