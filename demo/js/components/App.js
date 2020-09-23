e1cf5eb72b51415a3a6966ead0f24a5a13f4953b
Adele D <adele.delamarche@gmail.com>
2020-09-23 22:15:24 +0200

    Move the App component to its own file

---
+++ demo/js/components/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactPaginate from 'react-paginate';
+import $ from 'jquery';
+
+import { CommentList } from './CommentList';
+
+export class App extends Component {
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    perPage: PropTypes.number.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      offset: 0,
+    };
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      data: { limit: this.props.perPage, offset: this.state.offset },
+      dataType: 'json',
+      type: 'GET',
+
+      success: (data) => {
+        this.setState({
+          data: data.comments,
+          pageCount: Math.ceil(data.meta.total_count / data.meta.limit),
+        });
+      },
+
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString()); // eslint-disable-line
+      },
+    });
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+  }
+
+  handlePageClick = (data) => {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * this.props.perPage);
+
+    this.setState({ offset: offset }, () => {
+      this.loadCommentsFromServer();
+    });
+  };
+
+  render() {
+    return (
+      <div className="commentBox">
+        <CommentList data={this.state.data} />
+        <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
+      </div>
+    );
+  }
+}
--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,83 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import ReactPaginate from 'react-paginate';
-import $ from 'jquery';
 
-import { CommentList } from './components/CommentList';
+import { App } from './components/App';
 
 window.React = React;
-
-export class App extends Component {
-  static propTypes = {
-    url: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    perPage: PropTypes.number.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: [],
-      offset: 0,
-    };
-  }
-
-  loadCommentsFromServer() {
-    $.ajax({
-      url: this.props.url,
-      data: { limit: this.props.perPage, offset: this.state.offset },
-      dataType: 'json',
-      type: 'GET',
-
-      success: (data) => {
-        this.setState({
-          data: data.comments,
-          pageCount: Math.ceil(data.meta.total_count / data.meta.limit),
-        });
-      },
-
-      error: (xhr, status, err) => {
-        console.error(this.props.url, status, err.toString()); // eslint-disable-line
-      },
-    });
-  }
-
-  componentDidMount() {
-    this.loadCommentsFromServer();
-  }
-
-  handlePageClick = (data) => {
-    let selected = data.selected;
-    let offset = Math.ceil(selected * this.props.perPage);
-
-    this.setState({ offset: offset }, () => {
-      this.loadCommentsFromServer();
-    });
-  };
-
-  render() {
-    return (
-      <div className="commentBox">
-        <CommentList data={this.state.data} />
-        <ReactPaginate
-          previousLabel={'previous'}
-          nextLabel={'next'}
-          breakLabel={'...'}
-          breakClassName={'break-me'}
-          pageCount={this.state.pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={this.handlePageClick}
-          containerClassName={'pagination'}
-          subContainerClassName={'pages pagination'}
-          activeClassName={'active'}
-        />
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
   <App url={'http://localhost:3000/comments'} author={'adele'} perPage={10} />,
